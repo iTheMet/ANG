24e735142c4ded57d00914f7e8971c703d20f9f1
shnur1888 <80851093+shnur1888@users.noreply.github.com>
2021-06-08 04:52:11 +0300

    Переделал отображение доски в консоли
    Скинул создание и отображение доски  в класc Game
     Методы
    setGame
    showGame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-	  BordTriangke[] Bord = new BordTriangke[24];
-	 for (int i = 0;i<24;i++ ){
-		 BordTriangke test = new BordTriangke();
-	 	Bord[i]=test;
-	 	System.out.println(Bord[i]);
-	 }
-	 Bord[23].setNumOfStones(15);
-	 Bord[23].setColorOfStones('W');
-	 Bord[23].setEmpty(false);
-	 Bord[11].setNumOfStones(15);
-	 Bord[11].setColorOfStones('B');
-	 Bord[11].setEmpty(false);
-
+	 Game.setGame();
+	 Game.showGame();
 
 
 
